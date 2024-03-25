7fb01684f6aa5f44404083a181ff2e7b0c90833f
alan <alan@iMac-de-alan.local>
2024-03-25 14:12:56 -0600

    add connect to db function and uploaded seeds
    the recipes array didnt upload any seed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import "dotenv/config.js"
 import express from 'express'
+import dbConnection from "./config/connectDB.js"
+import seedDB from "./config/seedDB.js"
 
 const app = express()
 
@@ -7,14 +9,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+
+dbConnection()
+
+
+seedDB(true)
+
 const PORT = 4001
 
 app.listen(PORT, () => {
     console.log(`conectado al puerto ${PORT}`)
 })
-
-//crear modelo 1 y modelo N
-//Cocina y Recetas
-//la cocina italiana tiene pizzas, pastas, la mexicana tacos, mole, enchiladas
-
-//
---
+++ config/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose  from "mongoose";
+import seedDB from "./seedDB.js";
+
+const dbConnection = async () => {
+    try {
+        mongoose.connect(process.env.MONGO)
+        console.log('conectado a la db')
+    } catch (error) {
+        console.error(`error al conectarse a la db ${error.message}`)
+    }
+}
+
+export default dbConnection
