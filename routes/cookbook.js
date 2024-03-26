304c88c9d8f81481252cc6b2d07a0c379b2fe05c
alan <alan@iMac-de-alan.local>
2024-03-25 23:26:45 -0600

    add names for cookbook routes and error handler for cookbook routes
    controller for post, put and delete are missing

--- routes/cookbook.js
+++ routes/cookbook.js
@@ -7,4 +7,18 @@ router.get('/', getAllCookBooks)
 
 router.get('/:id', getCookBookById)
 
+router.post('/add')
+
+router.put('/edit/:id')
+
+router.put('/:id/update-recipes')
+
+router.delete('/delete/:id')
+
+router.use((req, res, next) => {
+    const error = new Error('No encontramos el libro de cocina')
+    error.status = 404
+    next(error)
+})
+
 export default router
