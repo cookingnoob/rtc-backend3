db3d2e2c070e162762d2beed55c47cf0dbc82782
alan <alan@iMac-de-alan.local>
2024-03-29 11:42:27 -0600

    deleted isDeleteCookbook from put controller
    it already replaces an existing cookbook in the recipe

--- controller/recipes.js
+++ controller/recipes.js
@@ -49,7 +49,7 @@ const postNewRecipe = async (req, res, next) => {
           console.log('se agrego un libro existente')
           res.status(200).json({ data: newRecipe });
     }else{
-         const newCookBook = await new CookBooks({ title: cookbook,});
+         const newCookBook = await new CookBooks({ title: cookbook});
          await newCookBook.save();
          console.log('se creó un libro nuevo')
          const newRecipe = new Recipes({
@@ -104,7 +104,7 @@ const putEditRecipe = async (req, res, next) => {
 const putEditCookbookInRecipe = async (req, res, next) => {
     try {
       const {id} = req.params
-      const {name, cookbook, steps, ingredients, deleteCookbook} = req.body
+      const {name, cookbook, steps, ingredients} = req.body
 
       //checa si hay valores que no sean cookbook y lanza error
       if(name || steps || ingredients){
@@ -113,31 +113,33 @@ const putEditCookbookInRecipe = async (req, res, next) => {
         next(error)
         return
       }
-    
+      
       const isCookbookInDB = await CookBooks.findOne({title: cookbook})
-
       //busca la receta 
       const recipe = await Recipes.findById(id)
 
-      //si el librio existe en la db agrega el id
+     
       if(isCookbookInDB){
+        //checa si el libro existente en db ya esta ligado a la receta
         if(recipe.cookbook.toString() === isCookbookInDB._id.toString()){
           const error = new Error('La receta ya tiene ese libro')
           error.status = 409
           next(error)
+        //en caso de que no los liga por el id
         }else {
           recipe.cookbook = isCookbookInDB._id
+          isCookbookInDB.recipes.push(id)
+          await isCookbookInDB.save()
           await recipe.save()
           res.status(200).json({data: 'se agrego el libro a la receta'})
         }
       }else{
         //si no existe en la db crea una instancia y agrega el id 
-        const newCookBook = await new CookBooks({title: cookbook})
+        const newCookBook = await new CookBooks({title: cookbook, recipes: id})
         await newCookBook.save()
         recipe.cookbook = newCookBook._id
         await recipe.save()
         res.status(200).json({data: 'se agrego el libro'})
-        //falta linkear la receta al cookbook
       }
     } catch (error) {
       next(error)
