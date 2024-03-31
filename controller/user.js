d8d0a5211aaa720fdebcf9d77d40ad6c9373d24d
alan <alan@iMac-de-alan.local>
2024-03-31 13:24:34 -0600

    add login user controller
    it checks for email and password to be in db.
    
    need to implement jwt to the login process

--- controller/user.js
+++ controller/user.js
@@ -6,6 +6,7 @@ const registerUser = async (req, res, next) => {
         const {email, password} = req.body
 
         const userExists = await Users.findOne({email: email})
+
         if(userExists){
             const error = new Error ('ya existe ese usuario')
             error.status = 400
@@ -20,11 +21,39 @@ const registerUser = async (req, res, next) => {
                                     password: hashedPassword
                                 })
         await newUser.save()
-        res.status(200).json({data: `se creo el usuario ${email} hashed password ${hashedPassword}`})
+        res.status(200).json({data: `se creo el usuario ${email}`})
 
     } catch (error) {
         next(error)
     }
 }
 
-export {registerUser}
+const loginUser = async(req, res, next) => {
+    try {
+        const {email, password} = req.body
+
+        const userInDB = await Users.findOne({email: email})
+
+        if(!userInDB){
+            const error = new Error('Verifica tus datos para iniciar sesion')
+            error.status = 404
+            next(error)
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, userInDB.password)
+
+        if(!isPasswordCorrect){
+            const error = new Error('Verifica tus datos para iniciar sesion')
+            error.status = 404
+            next(error)
+        }
+
+        if(userInDB && isPasswordCorrect){
+            res.status(200).json({data: 'datos correctos para iniciar sesion'})
+        }
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {registerUser, loginUser}
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { registerUser } from '../controller/user.js'
+import { loginUser, registerUser } from '../controller/user.js'
 
 const router = express.Router()
 
 router.post("/create-account", registerUser)
+
+router.post("/login", loginUser)
 
 export default router
